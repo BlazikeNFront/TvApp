f9b05f57b4c509cfad692e0966f219d55904531e
unknown <damian9691@wp.pl>
2020-09-20 18:30:04 +0200

    Fixed  detailed View code and styles

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 
 
 import {linkDOMElements} from '/src/linkDOMElem.js';
-import {getShows} from '/src/apiServ.js';
+import {getShows,getShowById} from '/src/apiServ.js';
 import {createDOMElement} from '/src/linkDOMElem.js';
+
+// trzeba naprawic zeby buttony showViews przy widoku detalied nie działały - buggują się
+// card trzeba naprawic widok na fixed aktulanie na tym sie buguje
 
 class tvApp{
     constructor(){
@@ -11,7 +14,7 @@ class tvApp{
         this.selectedName = 'Dog';
         this.initApp();
         this.eventListerners();
-       
+        this.eventLisCheck = false;
     }
 
     initApp = () => {
@@ -19,7 +22,7 @@ class tvApp{
        const listDataGenre = Array.from(document.querySelectorAll('[data-genre]')).map(item => item.dataset.genre);
       
        this.domElements = linkDOMElements('grab',listDataGrab);
-       console.log()
+     
        this.showNameButton = linkDOMElements('genre',listDataGenre);
        
     }
@@ -32,8 +35,22 @@ class tvApp{
                
             }
 
-           this.showNameButton[item].addEventListener('click', this.selectedTag)
+           this.showNameButton[item].addEventListener('click', this.selectedTag)   // what about this event listener???should be removed or no??? i bet not
         }
+    }
+    openDetails = () => {
+      const showId = event.target.dataset
+      
+      getShowById(showId['id']).then(data => {
+        console.log(data)
+        const singeCard = this.createShowCard(data,true);
+        this.domElements.showPreview.appendChild(singeCard);
+        this.domElements.showPreview.style.display = 'block';
+      this.domElements.showPreview.querySelector('.card').style.width = '80%';
+      this.domElements.showPreview.querySelector('.card').style.maxWidth = '700px';
+      
+      })
+      
     }
 
      displayTvShow = () => {
@@ -41,21 +58,38 @@ class tvApp{
          
         }
          
-      createShowCard = (show) => {
+      createShowCard = (show, showDetails) => {
         let image;
         let showSummary;
+        let buttonValue;
         if(show.image === null){
           image = 'https://picsum.photos/220/309'
         }
-          else {image = show.image.medium }    // check if image exist if not give random img from lorem picsum(need more specific placcehoder???)
+             // check if image exist if not give random img from lorem picsum(need more specific placcehoder???)
        if(show.summary === null){
-            imashowSummaryge = 'There is no summary for that show yet'
+            showSummary = 'There is no summary for that show yet';
           }
-          const arr = Array.from(show.summary.split(" ").splice(0,30)); //shortens of summaries
-          arr.push('...')
-          const shortShowSummary = arr.join(" ");
-        
-           
+          else {
+            showSummary = show.summary
+          }
+          
+          
+          let shortShowSummary
+
+          if (showDetails){
+            shortShowSummary = showSummary;
+            image = show.image.original
+            buttonValue = 'Close card'
+
+          }
+          else {
+            const arr = Array.from(show.summary.split(" ").splice(0,30)); //shortens of summaries
+             arr.push('...');
+            shortShowSummary = arr.join(' ');
+            image = image = show.image.medium
+            buttonValue = 'Show more...'
+          }
+          
         
         const divCard = createDOMElement('div','card',);
         
@@ -63,7 +97,7 @@ class tvApp{
         const div = createDOMElement('div','card-body');
         const h5 = createDOMElement('h5','card-title',show.name);
         const p = createDOMElement('p','card-text',shortShowSummary);
-        const button = createDOMElement('button',',btn btn-primary','Show more...');
+        const button = createDOMElement('button',',btn btn-primary buttonInShowList',buttonValue);
 
         
         divCard.appendChild(img);
@@ -72,14 +106,34 @@ class tvApp{
         div.appendChild(p);
         div.appendChild(button);
 
-        this.domElements.showWrapper.appendChild(divCard)
+        button.dataset.id = show.id;
+        if(showDetails){
+          button.addEventListener('click', this.closeDetails)
+          
+         }
+          else {
+          button.addEventListener('click', this.openDetails)
+          }
+        //
+        
+        return divCard;
+      }
+
+      closeDetails = () => {
+       const closeBtn =  this.domElements.showPreview.querySelector('[data-id]');
+        closeBtn.removeEventListener('click',this.closeDetails);
+        this.domElements.showPreview.style.display = 'none';
+        this.domElements.showPreview.innerHTML = '';
+       
       }
 
       renderShowList = (shows) => {
         this.domElements.showWrapper.innerHTML = '';
+        Array.from(document.querySelectorAll('[data-id')).forEach(button => button.removeEventListener('click',this.openDetails));
 
       for (let {show}of shows){
-        this.createShowCard(show)
+       const card = this.createShowCard(show,false);
+        this.domElements.showWrapper.appendChild(card)
       }
       }
 }
